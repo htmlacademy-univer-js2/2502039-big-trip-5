f9f170197a7812479b54fdbe4a097bcf5b3529b5
Lukyashka02 <alina.lukyanenko02@yandex.com>
2025-05-24 11:51:52 +0500

    module5-task1

--- src/model/point-model.js
+++ src/model/point-model.js
@@ -1,32 +1,9 @@
-import { mockDestinations } from '../mock/destination';
-import { mockOffers } from '../mock/offer';
-import { getShufflePoints } from '../mock/point';
-
-const FIRST_ELEMENT = 0;
-
+import { mockPoints } from '../mock/point';
+import { toCamelCase } from '../utils/common';
 export default class PointModel{
-  points = getShufflePoints();
-  pointOffers = mockOffers;
-  destinations = mockDestinations;
+  #points = mockPoints.map((point)=>toCamelCase(point));
 
-  getPoints(){
-    return this.points;
-  }
-
-  getOffers(){
-    return this.pointOffers;
-  }
-
-  getDestinations(){
-    return this.destinations;
-  }
-
-  getOfferById(type, id){
-    return id ? this.pointOffers.filter((offer)=> offer.type === type)[FIRST_ELEMENT].offers.find((item)=>item.id === id)
-      : this.pointOffers.filter((offer)=> offer.type === type)[FIRST_ELEMENT];
-  }
-
-  getDestinationById(id){
-    return this.destinations.find((item)=>item.id === id);
+  get points(){
+    return this.#points;
   }
 }
